3da682ab9a3660755009fdb659a227f7d996ca98
Yi DENG <michaelyideng@gmail.com>
2018-09-19 16:59:23 +0800

    click MenuItem to switch content

--- src/Components/BasicLayout.jsx
+++ src/Components/BasicLayout.jsx
@@ -8,17 +8,23 @@ export default class BasicLayout extends React.Component {
     super(props);
     this.state = {
       collapsed: false,
+      contentKey: 1,
     };
     this.onCollapse = this.onCollapse.bind(this);
+    this.switchContent = this.switchContent.bind(this);
   }
 
   onCollapse(collapsed) {
     this.setState({ collapsed });
   }
 
+  switchContent(contentKey) {
+    this.setState({ contentKey });
+  }
+
   render() {
     return (
-      <Layout>
+      <Layout >
         <Sider
           collapsible
           collapsed={this.state.collapsed}
@@ -27,15 +33,15 @@ export default class BasicLayout extends React.Component {
         >
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
+            <Menu.Item key="1" onClick={() => this.switchContent(1)}>
               <Icon type="user" />
               <span>区块链数据看板</span>
             </Menu.Item>
-            <Menu.Item key="2">
+            <Menu.Item key="2" onClick={() => this.switchContent(2)}>
               <Icon type="video-camera" />
               <span>智能合约调用</span>
             </Menu.Item>
-            <Menu.Item key="3">
+            <Menu.Item key="3" onClick={() => this.switchContent(3)}>
               <Icon type="upload" />
               <span>智能合约安装</span>
             </Menu.Item>
@@ -43,11 +49,9 @@ export default class BasicLayout extends React.Component {
         </Sider>
 
         <Layout>
-          <Header style={{ background: '#fff' }}>
-            标题
-          </Header>
-          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 1920 }}>
-            内容
+          {/* 在此配置内容key对应的内容类，相当于路由 */}
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff' }}>
+            {this.state.contentKey}
           </Content>
         </Layout>
 
