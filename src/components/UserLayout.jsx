5349db6eaa35728140729902f8c68bd0c8412b65
IamChuancey <744919277@qq.com>
2018-10-22 18:05:35 +0800

    16、优化登录界面

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -2,7 +2,7 @@
 
 // TODO: 登录页（秘钥导入页面）
 import React from 'react';
-import { Button, Input, Layout, Avatar } from 'antd';
+import { Button, Input, Layout, Icon } from 'antd';
 import getFabricClientSingleton from '../util/fabric';
 import { getConfigDBSingleton } from '../util/createDB';
 
@@ -11,6 +11,10 @@ const logger = require('electron-log');
 const db = getConfigDBSingleton();
 
 const { Content } = Layout;
+
+const path = require('path');
+
+const bcgd = path.join(__dirname,'content/styles/image/blc.jpg');
 
 export default class DataContent extends React.Component {
   constructor(props) {
@@ -22,8 +26,9 @@ export default class DataContent extends React.Component {
       username: 'Org1Admin',
       certPath: '',
       keyPath: '',
+      certlabel: 'choose a certificate',
+      keylabel: 'choose a privary key',
     };
-
 
     this.onClick = this.onClick.bind(this);
     this.peerChange = this.peerChange.bind(this);
@@ -56,12 +61,16 @@ export default class DataContent extends React.Component {
   cerImport() {
     const selectedFile = document.getElementById('cerFiles').files[0];// 获取读取的File对象
     this.setState({ certPath: selectedFile.path });
+    const cerArray = selectedFile.path.split('/');
+    this.setState({ certlabel: cerArray[cerArray.length - 1] });
   }
 
 
   priImport() {
     const selectedFile = document.getElementById('priFiles').files[0];// 获取读取的File对象
     this.setState({ keyPath: selectedFile.path });
+    const priArray = selectedFile.path.split('/');
+    this.setState({ keylabel: priArray[priArray.length - 1] });
   }
 
   ordererChange(event) {
@@ -74,46 +83,110 @@ export default class DataContent extends React.Component {
 
 
   render() {
+    const LayoutStyle = {
+      maxHeight: '600px',
+      maxWidth: '800px',
+    };
+    const backgroundStyle = {
+      width: '400px',
+      height: 'auto',
+      minHeight: '900px',
+      display: 'block',
+      position: 'absolute',
+      backgroundImage: 'url('+bcgd+')',
+    };
+    const contentStyle = {
+      padding: '80px 26px',
+      backgroundColor: '#fff',
+      width: '400px',
+      height: 'auto',
+      minHeight: '900px',
+      display: 'block',
+      marginLeft: '400px',
+    };
+    const LoginStyle = {
+      display: 'block',
+      alignItems: 'center',
+      padding: '24px 0',
+      margin: '24px 0',
+    };
+    const cerfileStyle = {
+      width: '0.1px',
+      height: '0.1px',
+      opacity: 0,
+      overflow: 'hidden',
+      position: 'absolute',
+      zIndex: -1,
+    };
+    const prifilesStyle = {
+      width: '0.1px',
+      height: '0.1px',
+      opacity: 0,
+      overflow: 'hidden',
+      position: 'absolute',
+      zIndex: -1,
+    };
+    const labelStyle = {
+      fontSize: '1.1em',
+      border: '1px solid rgb(217, 217, 217)',
+      borderRadius: '4px',
+      display: 'block',
+      float: 'right',
+      width: '220px',
+      height: '32px',
+      verticalAlign: 'middle',
+      textAlign: 'center',
+      lineHeight: '30px',
+    };
+    const spanStyle = {
+      display: 'inlineBlock',
+      fontSize: '1.2em',
+    };
+    const InputStyle = {
+      width: '220px',
+      display: 'block',
+      float: 'right',
+    };
     return (
-      <Layout >
-        <Content style={{margin: '32px 16px',padding: '24px', background: '#fff', width: '50%', height: '100%',minHeight:'900px' }}>
-          <div style={{ margin: '32px 0' }}>
-            <center><Avatar size={64} icon="user" /></center>
-          </div>
+      <Layout style={LayoutStyle}>
 
-          <div style={{ margin: '24px 0' }}>
-            peer url：&thinsp;&thinsp;
-            <Input type="text" value={this.state.peerUrl} style={{ width: '75%' }} onChange={this.peerChange} />
-          </div>
+        <div style={backgroundStyle} >
+          <image src={bcgd} alt=" "/>
+        </div>
 
-          <div style={{ margin: '24px 0' }}>
-            orderer url：&thinsp;
-            <Input type="text" value={this.state.ordererUrl} style={{ width: '75%' }} onChange={this.ordererChange} />
-          </div>
-
-          <div style={{ margin: '24px 0' }}>
-            username：&thinsp;&thinsp;
-            <Input type="text" value={this.state.username} style={{ width: '75%' }} onChange={this.usernameChange} />
-          </div>
-
-          <div style={{ margin: '24px 0' }}>
-            certificate：
-            <input type="file" id="cerFiles" style={{ width: '75%' }} onChange={this.cerImport} />
-          </div>
-
-          <div style={{ margin: '24px 0' }}>
-            private key：
-            <input type="file" id="priFiles" style={{ width: '75%' }} onChange={this.priImport} />
-          </div>
-
-          <div style={{ margin: '24px 0' }}>
-            <Button type="primary" style={{ width: '100%' }} onClick={this.onClick}>登录</Button>
-          </div>
-
-        </Content>
+        <div style={contentStyle}>
+          <Content>
+            <div style={{ LoginStyle }}>
+              <span style={{ fontSize: '38px' }}>Fabric Desktop</span>
+            </div>
+            <div style={{ margin: '36px 8px 29px 8px' }}>
+              <span style={spanStyle}>peer &thinsp;&thinsp;url:</span>
+              <Input type="text" style={InputStyle} value={this.state.peerUrl} onChange={this.peerChange} />
+            </div>
+            <div style={{ margin: '29px 8px' }}>
+              <span style={spanStyle}>orderer url:</span>
+              <Input type="text" style={InputStyle} value={this.state.ordererUrl} onChange={this.ordererChange} />
+            </div>
+            <div style={{ margin: '29px 8px' }}>
+              <span style={spanStyle}>username:</span>
+              <Input type="text" style={InputStyle} value={this.state.username} onChange={this.usernameChange} />
+            </div>
+            <div style={{ margin: '29px 8px' }}>
+              <span style={spanStyle}>certificate：</span>
+              <input type="file" id="cerFiles" name="cerFiles" style={cerfileStyle}onChange={this.cerImport} />
+              <label htmlFor="cerFiles" style={labelStyle}><Icon type="upload" theme="outlined" />&thinsp;{this.state.certlabel} </label>
+            </div>
+            <div style={{ margin: '29px 8px' }}>
+              <span style={spanStyle}>private key：</span>
+              <input type="file" id="priFiles" name="priFiles" style={prifilesStyle} onChange={this.priImport} />
+              <label htmlFor="priFiles" style={labelStyle}><Icon type="upload" theme="outlined" />&thinsp;{this.state.keylabel}</label>
+            </div>
+            <div style={{ margin: '32px 8px' }}>
+              <Button type="primary" style={{ width: '100%' }} onClick={this.onClick}>登录</Button>
+            </div>
+          </Content>
+        </div>
       </Layout>
-
-
     );
   }
 }
